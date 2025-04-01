ef381d8fea3924a27847aaed7bfa7a28696059f8
Daniel Kim <k.danielnc@gmail.com>
2025-04-01 16:20:44 -0500

    slight firebase tweaks

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Firebase configuration
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDFVw_VDzuIJWWGv9iW70lyxJdtWgIspio",
+  authDomain: "robowhales-scouting.firebaseapp.com",
+  projectId: "robowhales-scouting",
+  storageBucket: "robowhales-scouting.firebasestorage.app",
+  messagingSenderId: "94724192757",
+  appId: "1:94724192757:web:270a356595fdddc54b08bc",
+  measurementId: "G-RW32SXHSRX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { app, db }; 
